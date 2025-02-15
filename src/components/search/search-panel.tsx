474c60fc7309af5c2e87de5455b700d1e2e09450
Richard Nguyen <richard@richardhnguyen.com>
2025-02-14 23:15:58 -0800

    feat: add autocomplete for controlling search UI

--- src/components/search/search-panel.tsx
+++ src/components/search/search-panel.tsx
@@ -1,72 +1,107 @@
 "use client";
 
 import React from "react";
-import { useInstantSearch } from "react-instantsearch";
-import { useRouter } from "next/navigation";
+import { createAutocomplete } from "@algolia/autocomplete-core";
+import { getAlgoliaResults } from "@algolia/autocomplete-preset-algolia";
 
-import SearchBox from "./search-box";
-import SearchResult from "./search-result";
-import SearchEmptyQueryBoundary from "./search-empty-boundary";
-import SearchNoResultsBoundary from "./search-no-result-boundary";
+import { client as searchClient } from "@/lib/algolia";
+import {
+  InternalSearchHitWithParent,
+  InternalSearchState,
+} from "@/types/algolia";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 type Props = {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
-}
+};
 
 function SearchPanel({ onClose }: Props): React.JSX.Element {
-  const { results } = useInstantSearch();
-  const panelRef = React.useRef<HTMLDivElement>(null);
   const { push } = useRouter();
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [autocompleteState, setAutocompleteState] = React.useState<
+    InternalSearchState<InternalSearchHitWithParent>
+  >({
+    query: "",
+    isOpen: false,
+    activeItemId: null,
+    collections: [],
+    status: "idle",
+    context: {},
+    completion: null,
+  });
 
-  const [activeHit, setActiveHit] = React.useState<number>(0);
-  const hitRefs = React.useMemo(
+  const autocomplete = React.useMemo(
     () =>
-      Array.from({ length: results.hits.length }, () =>
-        React.createRef<HTMLAnchorElement>()
-      ),
-    [results.hits]
+      createAutocomplete<
+        InternalSearchHitWithParent,
+        React.FormEvent<HTMLFormElement>,
+        React.MouseEvent,
+        React.KeyboardEvent
+      >({
+        onStateChange({ state }) {
+          // (2) Synchronize the Autocomplete state with the React state.
+          setAutocompleteState(state);
+        },
+        navigator: {
+          navigate({ itemUrl}) {
+            push(itemUrl);
+            onClose();
+          }
+        },
+        getSources({ query }) {
+          if (!query) {
+            return [];
+          }
+
+          return [
+            // (3) Use an Algolia index source.
+            {
+              sourceId: "article",
+              getItemInputValue({ item }) {
+                return item.query as string;
+              },
+              getItems({ query }) {
+                return getAlgoliaResults({
+                  searchClient,
+                  queries: [
+                    {
+                      indexName: process.env
+                        .NEXT_PUBLIC_ALGOLIA_INDEX_NAME as string,
+                      params: {
+                        query,
+                        hitsPerPage: 4,
+                        highlightPreTag: "<mark>",
+                        highlightPostTag: "</mark>",
+                        snippetEllipsisText: "...",
+                        attributesToRetrieve: [
+                          "title",
+                          "imageUrl",
+                          "description",
+                          "tags",
+                          "headings",
+                          "publishedAt",
+                        ],
+                        attributesToSnippet: ["description:20", "headings:20"],
+                      },
+                    },
+                  ],
+                });
+              },
+              getItemUrl({ item }) {
+                return `/blog/${item.objectID}`;
+              },
+            },
+          ];
+        },
+      }),
+    [onClose, push]
   );
 
-  const handleKeyDown = React.useCallback((evt: React.KeyboardEvent<HTMLDivElement>) => {
-    const key = evt.key;
-
-    if (key === "ArrowDown") {
-      evt.preventDefault();
-
-      const nextHit = activeHit + 1;
-      if (nextHit < hitRefs.length) {
-        setActiveHit(nextHit);
-      }
-
-    } else if (key === "ArrowUp") {
-      evt.preventDefault();
-
-      const prevHit = activeHit - 1;
-
-      if (prevHit >= 0) {
-        setActiveHit(prevHit);
-      }
-
-    } else if (key === "Enter") {
-      evt.preventDefault();
-
-      const hit = hitRefs[activeHit].current;
-      if (hit) {
-        onClose();
-        push(hit.href);
-      }
-    } else {
-      setActiveHit(0);
-    }
-  }, [activeHit, hitRefs, onClose, push]);
-
   return (
-    <div
-      ref={panelRef}
-      className="flex flex-col gap-2 p-4 rounded-lg border border-border bg-background"
-    >
+    <div className="flex flex-col gap-2 p-4 rounded-lg border border-border bg-background">
       <div className="flex items-center justify-between mt-4 md:mt-0">
         <h2 className="text-lg font-semibold">Search</h2>
         <p className="text-sm text-slate-500">
@@ -75,16 +110,59 @@ function SearchPanel({ onClose }: Props): React.JSX.Element {
       </div>
 
       <div className="flex flex-col gap-2">
-        <SearchBox onKeyDown={handleKeyDown} />
+        <form
+          className="aa-Form"
+          {...autocomplete.getFormProps({ inputElement: inputRef.current })}
+        >
+          <input
+            ref={inputRef}
+            className={cn(
+              "flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
+            )}
+            {...(autocomplete.getInputProps({
+              inputElement: inputRef.current!,
+              autoFocus: true,
+            }) as unknown as React.InputHTMLAttributes<HTMLInputElement>)}
+          />
+        </form>
 
-        <SearchEmptyQueryBoundary fallback={null}>
-          <SearchNoResultsBoundary>
-            <SearchResult activeHit={activeHit} setActiveHit={setActiveHit} hitRefs={hitRefs} />
-          </SearchNoResultsBoundary>
-        </SearchEmptyQueryBoundary>
+        <div className="aa-Panel" {...autocomplete.getPanelProps({})}>
+          {autocompleteState.isOpen &&
+            autocompleteState.collections.map((collection, index) => {
+              const { source, items } = collection;
+
+              return (
+                <div key={`source-${index}`} className="aa-Source">
+                  {items.length > 0 && (
+                    <ul className="aa-List" {...autocomplete.getListProps()}>
+                      {items.map((item) => {
+                        const itemProps = autocomplete.getItemProps({
+                          item,
+                          source,
+                          onClick: () => {
+                            push(`/blog/${item.objectID}`);
+                            onClose();
+                          }
+                        });
+
+                        return (
+                        <li
+                          key={item.objectID}
+                          className="aa-Item"
+                          {...itemProps}
+                        >
+                          {item.title}
+                        </li>
+                      )})}
+                    </ul>
+                  )}
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
 }
 
-export default React.memo(SearchPanel);
+export default SearchPanel;
--- src/types/algolia.ts
+++ src/types/algolia.ts
@@ -1,3 +1,9 @@
+import {
+  AutocompleteContext,
+  AutocompleteInsightsApi,
+  AutocompleteState,
+  BaseItem,
+} from "@algolia/autocomplete-core";
 import { type Frontmatter } from "./mdx";
 
 type Heading = {
@@ -5,11 +11,82 @@ type Heading = {
   text: string;
 };
 
-export interface AlgoliaAttributes {
+export interface InternalSearchAlgoliaAttributes {
   title: Frontmatter["title"];
   description: Frontmatter["description"];
   publishedAt: Frontmatter["publishedAt"];
   tags: Frontmatter["tags"];
   imageUrl: string;
   headings: Heading[];
+}
+
+interface InternalSearchHitAttributeHighlightResult {
+  value: string;
+  matchLevel: "full" | "none" | "partial";
+  matchedWords: string[];
+  fullyHighlighted?: boolean;
+}
+
+interface InternalSearchHitHighlightResult {
+  description: InternalSearchHitAttributeHighlightResult;
+  headings: Array<InternalSearchHitAttributeHighlightResult>;
+  tags: Array<InternalSearchHitAttributeHighlightResult>;
+  title: InternalSearchHitAttributeHighlightResult
+}
+
+interface InternalSearchHitAttributeSnippetResult {
+  value: string;
+  matchLevel: "full" | "none" | "partial";
+}
+
+interface InternalSearchHitSnippetResult {
+  description: InternalSearchHitAttributeSnippetResult
+  headings: Array<InternalSearchHitAttributeSnippetResult>;
+}
+
+export declare type InternalSearchHit = InternalSearchAlgoliaAttributes & {
+  objectID: string;
+  _highlightResult: InternalSearchHitHighlightResult;
+  _snippetResult: InternalSearchHitSnippetResult;
+  _rankingInfo?: {
+    promoted: boolean;
+    nbTypos: number;
+    firstMatchedWord: number;
+    proximityDistance?: number;
+    geoDistance: number;
+    geoPrecision?: number;
+    nbExactWords: number;
+    words: number;
+    filters: number;
+    userScore: number;
+    matchedGeoLocation?: {
+      lat: number;
+      lng: number;
+      distance: number;
+    };
+  };
+  _distinctSeqID?: number;
+  __autocomplete_indexName?: string;
+  __autocomplete_queryID?: string;
+  __autocomplete_algoliaCredentials?: {
+    appId: string;
+    apiKey: string;
+  };
+  __autocomplete_id?: number;
+};
+
+export type InternalSearchHitWithParent = InternalSearchHit & {
+  __parent: InternalSearchHitWithParent | null;
+  [key: string]: unknown;
+};
+
+interface InternalSearchContext extends AutocompleteContext {
+  algoliaInsightsPlugin?: {
+    insights: AutocompleteInsightsApi;
+  };
+}
+
+export interface InternalSearchState<TItem extends BaseItem>
+  extends AutocompleteState<TItem> {
+  context: InternalSearchContext;
 }
