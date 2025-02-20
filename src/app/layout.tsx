eedbdb4e1c13494b2984200fa81d674d0234d677
Richard Nguyen <richard@richardhnguyen.com>
2025-02-20 13:43:51 -0800

    feat: add theme script to prevent FOUC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,17 +20,77 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = sharedMetadata;
 
+const script = /*js*/ `
+void function () {
+  /**
+   * Callback fired when window.__theme was set or updated
+   */
+  window.__onThemeChange = function () {};
+
+  /**
+   * Sets the theme on the <body> element
+   * @param {string} newTheme - The new theme to set
+   */
+  let preferredTheme;
+  try {
+    preferredTheme = localStorage.getItem("theme");
+  } catch (err) { }
+
+  function setTheme(newTheme) {
+    const oldTheme = window.__theme;
+    const darkOrLight = 
+      newTheme === "system" 
+        ? (
+            window.matchMedia("(prefers-color-scheme: dark)").matches 
+            ? "dark" 
+            : "light"
+          ) 
+        : newTheme;
+
+    if (preferredTheme && document.documentElement.classList.contains(preferredTheme) && preferredTheme !== darkOrLight) {
+      document.documentElement.classList.replace(preferredTheme, darkOrLight);
+    } else {
+      document.documentElement.classList.add(darkOrLight);
+    }
+
+    window.__theme = newTheme;
+    preferredTheme = darkOrLight;
+    window.__onThemeChange(darkOrLight);
+  }
+
+  window.__setPreferredTheme = function (newTheme) {
+    setTheme(newTheme);
+    try {
+      localStorage.setItem("theme", newTheme);
+    } catch (err) {}
+  }
+
+  var darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  darkQuery.addListener(function (e) {
+    window.__setPreferredTheme(e.matches ? "dark" : "light");
+  });
+
+  setTheme(preferredTheme || (darkQuery.matches ? "dark" : "light"))
+}();
+`;
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" suppressHydrationWarning>
       <GoogleAnalytics gaId="G-WPL99G45VC" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <script
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html: script,
+          }}
+        ></script>
         <Header />
         {children}
         <Footer />
